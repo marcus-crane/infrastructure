be02d165b649f8fa6d616e892d8f6a02d9326d82
Marcus Crane <marcus@utf9k.net>
2023-02-04 13:34:47 +1300

    Properly use root zone

--- dns-home-utf9k-net.tf
+++ dns-home-utf9k-net.tf
@@ -1,13 +1,8 @@
-resource "cloudflare_zone" "home-utf9k-net-zone" {
-  account_id = var.cloudflare_account_id
-  zone       = "home.utf9k.net"
-}
-
-# CNAME Records
+# A Records
 
 ## Synology NAS
 resource "cloudflare_record" "a-home-utf9k-net" {
-  zone_id = cloudflare_zone.home-utf9k-net-zone.id
+  zone_id = cloudflare_zone.utf9k-net-zone.id
   name    = "home.utf9k.net"
   value   = "158.140.244.234"
   type    = "A"
