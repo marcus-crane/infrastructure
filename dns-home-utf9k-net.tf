a61ec809ef83310a92e488b366d05fd1b8977857
Marcus Crane <marcus@utf9k.net>
2023-02-17 23:47:11 +1300

    Add wildcard DNS record for Synology NAS
    Signed-off-by: Marcus Crane <marcus@utf9k.net>

--- dns-home-utf9k-net.tf
+++ dns-home-utf9k-net.tf
@@ -8,3 +8,11 @@ resource "cloudflare_record" "a-home-utf9k-net" {
   type    = "A"
   ttl     = 3600
 }
+
+resource "cloudflare_record" "a-wildcard-home-utf9k-net" {
+  zone_id = cloudflare_zone.utf9k-net-zone.id
+  name    = "*.home.utf9k.net"
+  value   = "158.140.244.234"
+  type    = "A"
+  ttl     = 3600
+}
