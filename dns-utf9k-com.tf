90c7e0130213acd8fa63125ee86800dd5d00128e
Marcus Crane <marcus@utf9k.net>
2023-06-27 22:34:46 +1200

    Fix up redirect phase
    Signed-off-by: Marcus Crane <marcus@utf9k.net>

--- dns-utf9k-com.tf
+++ dns-utf9k-com.tf
@@ -13,3 +13,34 @@ resource "cloudflare_record" "cname-utf9k-com" {
   type    = "CNAME"
   ttl     = 1
 }
+
+resource "cloudflare_record" "a-www-utf9k-com" {
+  zone_id = cloudflare_zone.utf9k-com-zone.id
+  name    = "www.utf9k.com"
+  proxied = true
+  value   = "192.0.2.1"
+  type    = "A"
+  ttl     = 1
+}
+
+resource "cloudflare_ruleset" "utf9k_com_redirect" {
+  zone_id = cloudflare_zone.utf9k-com-zone.id
+  name    = "Redirect to utf9k.net"
+  kind    = "zone"
+  phase   = "http_request_dynamic_redirect"
+
+  rules {
+    action = "redirect"
+    action_parameters {
+      from_value {
+        status_code = 301
+        target_url {
+          value = "https://utf9k.net"
+        }
+        preserve_query_string = false
+      }
+    }
+    expression = "(http.host contains \"utf9k\")"
+    enabled    = true
+  }
+}
--- dns-utf9k-org.tf
+++ dns-utf9k-org.tf
@@ -13,3 +13,34 @@ resource "cloudflare_record" "cname-utf9k-org" {
   type    = "CNAME"
   ttl     = 1
 }
+
+resource "cloudflare_record" "a-www-utf9k-org" {
+  zone_id = cloudflare_zone.utf9k-org-zone.id
+  name    = "www.utf9k.org"
+  proxied = true
+  value   = "192.0.2.1"
+  type    = "A"
+  ttl     = 1
+}
+
+resource "cloudflare_ruleset" "utf9k_org_redirect" {
+  zone_id = cloudflare_zone.utf9k-org-zone.id
+  name    = "Redirect to utf9k.net"
+  kind    = "zone"
+  phase   = "http_request_dynamic_redirect"
+
+  rules {
+    action = "redirect"
+    action_parameters {
+      from_value {
+        status_code = 301
+        target_url {
+          value = "https://utf9k.net"
+        }
+        preserve_query_string = false
+      }
+    }
+    expression = "(http.host contains \"utf9k\")"
+    enabled    = true
+  }
+}
