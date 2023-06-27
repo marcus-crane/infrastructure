f42b63b659b6dffa3a4d8139cbbc8cc7e92b8c0c
Marcus Crane <marcus@utf9k.net>
2023-06-27 18:20:16 +1200

    Test CNAME to apex
    Signed-off-by: Marcus Crane <marcus@utf9k.net>

--- dns-utf9k-com.tf
+++ dns-utf9k-com.tf
@@ -2,3 +2,14 @@ resource "cloudflare_zone" "utf9k-com-zone" {
   account_id = var.cloudflare_account_id
   zone       = "utf9k.com"
 }
+
+# CNAME records
+
+resource "cloudflare_record" "cname-utf9k-com" {
+  zone_id = cloudflare_zone.utf9k-com-zone.id
+  name    = "utf9k.net"
+  proxied = true
+  value   = cloudflare_pages_project.utf9k-net.subdomain
+  type    = "CNAME"
+  ttl     = 1
+}
