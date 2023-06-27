50ea81840beaf4bc22b8ca968ef8657df7a42d0f
Marcus Crane <marcus@utf9k.net>
2023-06-27 18:22:37 +1200

    Fix up some references
    Signed-off-by: Marcus Crane <marcus@utf9k.net>

--- dns-utf9k-com.tf
+++ dns-utf9k-com.tf
@@ -7,9 +7,9 @@ resource "cloudflare_zone" "utf9k-com-zone" {
 
 resource "cloudflare_record" "cname-utf9k-com" {
   zone_id = cloudflare_zone.utf9k-com-zone.id
-  name    = "utf9k.net"
+  name    = "utf9k.com"
   proxied = true
-  value   = cloudflare_pages_project.utf9k-net.subdomain
+  value   = "utf9k.net"
   type    = "CNAME"
   ttl     = 1
 }
