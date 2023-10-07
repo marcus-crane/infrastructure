4e94867a44fd6e7b276bf76d94cedcb45d81ed3b
Marcus Crane <marcus@utf9k.net>
2023-10-07 15:04:36 +1300

    Create A record for khindex api
    Signed-off-by: Marcus Crane <marcus@utf9k.net>

---
+++ dns-khindex-utf9k-net.tf
@@ -0,0 +1,9 @@
+# A Records
+
+resource "cloudflare_record" "a-home-utf9k-net" {
+  zone_id = cloudflare_zone.utf9k-net-zone.id
+  name    = "khindex.utf9k.net"
+  value   = "5.78.105.134"
+  type    = "A"
+  ttl     = 3600
+}
