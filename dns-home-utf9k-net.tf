fa893c84b756a334a848b08415902aa114c7bbb8
Marcus Crane <marcus@utf9k.net>
2023-12-11 13:46:44 +1300

    Updated home static IP address
    Signed-off-by: Marcus Crane <marcus@utf9k.net>

--- dns-home-utf9k-net.tf
+++ dns-home-utf9k-net.tf
@@ -4,7 +4,7 @@
 resource "cloudflare_record" "a-home-utf9k-net" {
   zone_id = cloudflare_zone.utf9k-net-zone.id
   name    = "home.utf9k.net"
-  value   = "210.54.238.178"
+  value   = "103.193.138.188"
   type    = "A"
   ttl     = 3600
 }
@@ -12,7 +12,7 @@ resource "cloudflare_record" "a-home-utf9k-net" {
 resource "cloudflare_record" "a-wildcard-home-utf9k-net" {
   zone_id = cloudflare_zone.utf9k-net-zone.id
   name    = "*.home.utf9k.net"
-  value   = "210.54.238.178"
+  value   = "103.193.138.188"
   type    = "A"
   ttl     = 3600
 }
